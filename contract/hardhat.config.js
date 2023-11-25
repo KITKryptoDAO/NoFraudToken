74a29229117afc9035e33d532d1730db95aca52d
Zion <mrzz@tuta.io>
2023-11-25 15:19:20 +0100

    erc712 demo

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    sepolia: {
+      url: "https://ethereum-sepolia.blockpi.network/v1/rpc/public",
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
