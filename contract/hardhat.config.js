07548bd2f5467117d3390eb7c7f6081a3e6c50bd
Zion <mrzz@tuta.io>
2023-12-06 11:29:51 +0100

    address config

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -6,17 +6,24 @@ require("@nomicfoundation/hardhat-ethers")
 require("dotenv").config();
 // require("./tasks");
 
+const address = {
+  "mumbai": {
+    "HuoToken": "0x7BB751f3090f9E42984B441ceCB8C6Ed21Ea4002",
+    "NoFraudToken": "0x3BfFbf7328A7370E52532f99f2ee348CFAFf190C",
+
+  },
+  "sepolia": {
+    "HuoToken": "0xb5364607B81E4AD9a13B1104975914b1E35CD891",
+    "NoFraudToken": "0x2794219fdE28c973B08509eFeBf0401F058a6543",
+  }
+}
+
 task("mint", "Mints a token")
   .addParam("to", "The address of the recipient")
   .addParam("tokenId", "The ID of the token")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
-    let tokenAddress = ""
-    if (hre.network.name === "mumbai") {
-      tokenAddress = "0x7BB751f3090f9E42984B441ceCB8C6Ed21Ea4002"
-    } else if (hre.network.name === "sepolia") {
-      tokenAddress = "0xb5364607B81E4AD9a13B1104975914b1E35CD891"
-    }
+    const tokenAddress = address[hre.network.name]["HuoToken"]
     const huo = await hre.ethers.getContractAt("HuoToken", tokenAddress);
     const tx = await huo.mint(taskArgs.to, taskArgs.tokenId)
     console.log(tx.hash)
@@ -28,12 +35,7 @@ task ("approve-all", "Approves all tokens")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
     console.log(await ethers.provider.getTransactionCount(signer[0].address))
-    let tokenAddress = ""
-    if (hre.network.name === "mumbai") {
-      tokenAddress = "0x7BB751f3090f9E42984B441ceCB8C6Ed21Ea4002"
-    } else if (hre.network.name === "sepolia") {
-      tokenAddress = "0xb5364607B81E4AD9a13B1104975914b1E35CD891"
-    }
+    const tokenAddress = address[hre.network.name]["HuoToken"]
     const huo = await hre.ethers.getContractAt("HuoToken", tokenAddress);
     const tx = await huo.setApprovalForAll(taskArgs.to, true)
     console.log(tx.hash)
@@ -45,12 +47,7 @@ task("approve", "Approves a token")
   .addParam("tokenId", "The ID of the token")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
-    let tokenAddress = ""
-    if (hre.network.name === "mumbai") {
-      tokenAddress = "0x7BB751f3090f9E42984B441ceCB8C6Ed21Ea4002"
-    } else if (hre.network.name === "sepolia") {
-      tokenAddress = "0xb5364607B81E4AD9a13B1104975914b1E35CD891"
-    }
+    const tokenAddress = address[hre.network.name]["HuoToken"]
     const huo = await hre.ethers.getContractAt("HuoToken", tokenAddress);
     const tx = await huo.approve(taskArgs.to, taskArgs.tokenId)
     console.log(tx.hash)
@@ -62,12 +59,7 @@ task("send", "Gives a token to the NoFraudToken contract")
   .addParam("tokenId", "The ID of the token")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
-    let nftAddress = ""
-    if (hre.network.name === "mumbai") {
-      nftAddress = "0x3BfFbf7328A7370E52532f99f2ee348CFAFf190C"
-    } else if (hre.network.name === "sepolia") {
-      nftAddress = "0x2794219fdE28c973B08509eFeBf0401F058a6543"
-    }
+    const nftAddress = address[hre.network.name]["NoFraudToken"]
     const nft = await hre.ethers.getContractAt("NoFraudToken", nftAddress);
     const tx = await nft.sendToBurn(taskArgs.nft, taskArgs.tokenId)
     console.log(tx.hash)
@@ -79,12 +71,7 @@ task("back", "Ask a token back")
   .addParam("tokenId", "The ID of the token")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
-    let nftAddress = ""
-    if (hre.network.name === "mumbai") {
-      nftAddress = "0x3BfFbf7328A7370E52532f99f2ee348CFAFf190C"
-    } else if (hre.network.name === "sepolia") {
-      nftAddress = "0x2794219fdE28c973B08509eFeBf0401F058a6543"
-    }
+    const nftAddress = address[hre.network.name]["NoFraudToken"]
     const nft = await hre.ethers.getContractAt("NoFraudToken", nftAddress);
     const tx = await nft.getBack(taskArgs.nft, taskArgs.tokenId)
     console.log(tx.hash)
@@ -97,12 +84,7 @@ task("transfer", "Transfers a token")
   .addParam("tokenId", "The ID of the token")
   .setAction(async (taskArgs, hre) => {
     const signer = await hre.ethers.getSigners();
-    let tokenAddress = ""
-    if (hre.network.name === "mumbai") {
-      tokenAddress = "0x7BB751f3090f9E42984B441ceCB8C6Ed21Ea4002"
-    } else if (hre.network.name === "sepolia") {
-      tokenAddress = "0xb5364607B81E4AD9a13B1104975914b1E35CD891"
-    }
+    const tokenAddress = address[hre.network.name]["HuoToken"]
     const huo = await hre.ethers.getContractAt("HuoToken", tokenAddress)
     const tx = await huo.safeTransferFrom(signer[0].address, taskArgs.to, taskArgs.tokenId)
     console.log(tx.hash)
