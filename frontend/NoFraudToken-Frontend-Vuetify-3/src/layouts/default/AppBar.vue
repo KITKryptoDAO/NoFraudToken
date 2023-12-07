26cb457305708272230371a9524d9987f61552bd
Yunhan Gao <hannes.gao@gmail.com>
2023-12-08 00:03:46 +0100

    Vue Components renamed

--- frontend/NoFraudToken-Frontend-Vuetify-3/src/layouts/default/AppBar.vue
+++ frontend/NoFraudToken-Frontend-Vuetify-3/src/layouts/default/AppBar.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app-bar flat>
     <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-6" />
-
-      Essentials Preset
+      <v-icon icon="fa-solid:burn" />
+      NoFraudToken Build v0.0.1
     </v-app-bar-title>
+    <w3m-button />
   </v-app-bar>
 </template>
 
