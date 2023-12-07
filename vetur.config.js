18a2958e7254c30069208a342e09b748d164739a
Yunhan Gao <hannes.gao@gmail.com>
2023-12-07 22:54:26 +0100

    Vuetify3 init

--- vetur.config.js
+++ vetur.config.js
@@ -11,21 +11,21 @@ module.exports = {
     // **optional** default: `[{ root: './' }]`
     // support monorepos
     projects: [
-      './frontend', // Shorthand for specifying only the project root location
+      './frontend/NoFraudToken-Frontend-Vuetify-3/', // Shorthand for specifying only the project root location
       {
         // **required**
         // Where is your project?
         // It is relative to `vetur.config.js`.
-        root: './frontend',
+        root: './frontend/NoFraudToken-Frontend-Vuetify-3/',
         // **optional** default: `'package.json'`
         // Where is `package.json` in the project?
         // We use it to determine the version of vue.
         // It is relative to root property.
-        package: './frontend/package.json',
+        package: './frontend/NoFraudToken-Frontend-Vuetify-3/package.json',
         // **optional**
         // Where is TypeScript config file in the project?
         // It is relative to root property.
-        tsconfig: './frontend/tsconfig.json',
+        tsconfig: './frontend/NoFraudToken-Frontend-Vuetify-3/tsconfig.json',
         // **optional** default: `'./.vscode/vetur/snippets'`
         // Where is vetur custom snippets folders?
         snippetFolder: './.vscode/vetur/snippets',
@@ -35,7 +35,7 @@ module.exports = {
         // It is relative to root property.
         // Notice: It won't actually do it. You need to use `require.context` or `Vue.component`
         globalComponents: [
-          './frontend/src/components/**/*.vue'
+          './frontend/NoFraudToken-Frontend-Vuetify-3/src/components/**/*.vue'
         ]
       }
     ]
